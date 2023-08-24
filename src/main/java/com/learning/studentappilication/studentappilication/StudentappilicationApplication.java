e9b7c9add9e5e04ac89a8003e5097a86cd1e45f9
sathish.kp <sathish.kp@zohocorp.com>
2023-08-24 06:45:13 +0530

    test

---
+++ src/main/java/com/learning/studentappilication/studentappilication/StudentappilicationApplication.java
@@ -0,0 +1,13 @@
+package com.learning.studentappilication.studentappilication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentappilicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentappilicationApplication.class, args);
+	}
+
+}
